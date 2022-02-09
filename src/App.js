10dde70c1de078354211fc7324a400c427064190
Christopher Hoppe <christopher.hoppe@volkswagen.de>
2022-02-09 11:54:52 +0100

    using API

--- src/App.js
+++ src/App.js
@@ -11,22 +11,44 @@ class App extends Component {
   state = {
     products:
       [
-        { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-        { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-        { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-        { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-        { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-        { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-        { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-        { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-        { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 }
+        
       ], 
     cartItemList: 
       [
-        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+        
       ]
+  }
+
+  async componentDidMount(){
+    const productsResponse = await fetch("http://localhost:8082/api/products");
+    const productsJson = await productsResponse.json()
+    const itemsResponse = await fetch("http://localhost:8082/api/items")
+    const itemsJson = await itemsResponse.json()
+    this.setState({
+      ...this.state,
+      products: productsJson
+    })
+    console.log(productsJson)
+    console.log(itemsJson)
+
+ 
+   
+    
+
+   const mappedItems =  itemsJson.map(item =>  {
+    Object.create(
+    ...item, 
+    product: productsJson.find(product => product.id === item.product_id))
+    });
+
+    console.log(mappedItems)
+
+   this.setState({
+     ...this.state,
+     cartItemList: mappedItems
+   })
+   
+
   }
 
  total = 0;
